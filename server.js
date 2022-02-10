261d05753f8d93a5e7e5d77b22024b7beb46605f
JJackson510 <jamiej.jj5@gmail.com>
2022-02-09 20:38:11 -0600

    add crud

--- server.js
+++ server.js
@@ -1,57 +1,111 @@
-const mysql = require('mysql2');
-const express = require('express');
+const mysql = require("mysql2");
+const express = require("express");
+const inputCheck = require("./utils/inputCheck");
+
 const port = process.env.PORT ||3001;
 const app = express();
 
+//Middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 // connect to databases
 const db = mysql.createConnection(
     {
-        host: 'localhost',
+        host: "localhost",
         // your mysql username,
-        user: 'root',
+        user: "root",
         // your mysql password
-        password: 'Jacksonstakeall0313',
-        database: 'election'
+        password: "Jacksonstakeall0313",
+        database: "election"
     },
-    console.log('Connected to the election database.')
+    console.log("Connected to the election database.")
 );
 
+//Get all candidates
+app.get("/api/candidates", (req, res) => {
+    const sql = `select * from candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({error: error.message});
+            return;
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+})
 
 // db.query(`select * from candidates`, (err,rows) => {
 //     console.log(rows);
 // });
 
-//Get a single candidate
-// db.query(`select * from candidates where id = 1`,(err, row) => {
-//     if(err) {
-//         console.error(err);
-//     }
-//     console.log(row);
-// });
+// Get a single candidate
+app.get("/api/candidate/:id", (req, res) => {
+  const sql = `select * from candidates where id =?`;
+const params = [req.params.id];
+
+db.query(sql, params, (err, rows) => {
+    if (err) {
+    res.status(400).json({ error: error.message });
+    return;
+    }
+    res.json({
+    message: "Success",
+    data: rows,
+    });
+});
+});
 
 //Delete a candidate
-// db.query(`delete from candidates where id = ?`, 1, (err, result) => {
-//     if(err) {
-//         console.error(err);
-//     }
-//     console.log(result);
-// });
-
-// Create a new candidate
-const sql = `insert into candidates (id,first_name,last_name, industry_connected)
-            values (?,?,?,?)`;
-
-const params = [1, 'Ronald', 'Firbank', 1];
+app.delete("/api/candidate/:id",(req, res) => {
+    const sql =`delete from candidates where id =?`;
+    const params =[req.params.id];
 
 db.query(sql, params, (err, result) => {
     if(err) {
-        console.error(err);
+        res.statusMessage(400).json({error: error.message});
+    } else if (!result.affectedRows) {
+        res.json ({
+            message: "Candidate can not be found"
+        });
+    }else {
+        res.json({
+            message: "deleted",
+            changes: result.affectedRows,
+            id: req.params.id
+        });
     }
-    console.log(result);
 });
+});
+
+// Create a new candidate
+app.post("/api/candidate", ({body}, res) => {
+    const errors =inputCheck(body, "first_name", "last_name", "industry_connected");
+    if(errors) {
+        res.status(400).json({error: errors});
+        return;
+    }
+
+
+    const sql = `insert into candidates (id,first_name,last_name, industry_connected)
+                values (?,?,?,?)`;
+
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({ error: err.message});
+        }
+        res.json ({
+            message: "success",
+            data: body
+        });
+    });
+});+
+
 
 app.use((req, res) => {
     res.status(404).end();
