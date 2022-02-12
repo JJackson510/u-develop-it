196ab887ddfe53fcef39eb64a7ea11cc47231d2b
JJackson510 <jamiej.jj5@gmail.com>
2022-02-11 18:56:48 -0600

    added voters table and voters

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 drop table if exists candidates;
 drop table if exists parties;
+drop table if exists voters;
 
 create table parties(
     id integer auto_increment primary key,
@@ -14,4 +15,12 @@ last_name VARCHAR(30) NOT NULL,
 party_id integer,
 industry_connected BOOLEAN NOT NULL,
 constraint fk_party foreign key (party_id) references parties(id) on delete set null
+);
+
+create table voters (
+    id integer auto_increment primary key,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    email varchar(50) not NULL,
+    create_at datetime default current_timestamp
 );
